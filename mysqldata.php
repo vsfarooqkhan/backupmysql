cb1df268989e4efe9870c1f289dda13e8cdb21e2
vsfarooqkhan <farooq@m.wid>
2018-11-24 12:21:37 +0530

    first commit

---
+++ mysqldata.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="backupmysql1.php" method="POST">
+Mysql host <input type="text" name="host"> <br>
+Mysql user name : <input type="text"  name="username"> <br>
+Password: <input type="password" name="password"> <br>
+Database name : <input type="text" name="dbname"> <br>
+<input type= "submit">
+</body>
+</html>
